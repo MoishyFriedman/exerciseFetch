515d3a34fc2f51f1f423d5d4629ae4bb10a5a68d
moishy friedman <moshehlf@gmail.com>
2023-08-13 15:26:51 +0300

    update exercise one tow

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 // 1.1
-async function fetchRequest() {
+async function fetchRequest(param) {
   try {
-    const data = await fetch("https://randomuser.me/api");
+    const data = await fetch("https://randomuser.me/api?" + param);
     if (data.ok) {
       return await data.json();
     } else {
@@ -19,23 +19,32 @@ const img = document.getElementsByTagName("img")[0];
 const pAge = document.getElementById("age");
 button.addEventListener("click", () => {
   fetchRequest().then((data) => {
-    pName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
-    pEmail.innerText = data.results[0].email;
+    pName.textContent = `${data.results[0].name.first} ${data.results[0].name.last}`;
+    pEmail.textContent = data.results[0].email;
     img.src = data.results[0].picture.thumbnail;
-    pAge.innerText = data.results[0].registered.age;
+    pAge.textContent = data.results[0].registered.age;
   });
 });
 
 // 1.2
 const buttonTow = document.getElementById("buttonTow");
 buttonTow.addEventListener("click", () => {
-  fetchRequest().then((data) => {
-    let i = 5;
-    while (i > 0) {
-      if (data.results[0].gender === "male") {
-        console.log(data);
-      }
-      i++;
-    }
-  });
+  fetchRequest("results=5&gender=male").then((data) => console.log(data));
 });
+
+// 2
+async function RequestJokes() {
+  const joke = document.getElementById("joke");
+  try {
+    const data1 = await fetch('https://api.humorapi.com/jokes/search?api-key=41429990cb2b4ce3a690d1457972f45d')
+    if (data1.ok) {
+      const finalData = await data1.json();
+      joke.textContent = finalData;
+    } else {
+      throw new Error("error");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+RequestJokes();
